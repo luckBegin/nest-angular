ca23808ea11b2186d814fef8828e724b97d79d7f
luckbegin <811140213@qq.com>
2018-09-05 21:00:17 +0800

    添加mysql链接

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module , NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'sysx',
+            synchronize: true,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
+
 })
 export class AppModule{}
