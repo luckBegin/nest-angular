42894627f7157cbc11bfcead43d30f49348211ce
Sirius.Black <ofghrt@qq.com>
2019-01-10 10:38:56 +0800

    新增 image entity 以及 controller , service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,23 @@
 import { Module , NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ImageManageModule} from "./modules/ImageMagage/ImageManage.module" ;
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: 'root',
-            database: 'sysx',
-            synchronize: true,
-        }),
-    ],
+    imports: [ ImageManageModule , TypeOrmModule.forRoot({
+		"type": "mysql",
+		"host": "localhost",
+		"port": 3306,
+		"username": "root",
+		"password": "root",
+		"database": "image",
+		"entities": [
+			"src/**/**.entity{.ts,.js}"
+		],
+		"synchronize": true
+	})],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [ AppService ],
 
 })
 export class AppModule{}
